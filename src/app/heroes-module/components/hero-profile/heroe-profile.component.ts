93a09d2723a3ea1ee9159d8902da42903c6583aa
Tjier Salazar <tjier.salazarvaros@gmail.com>
2020-10-07 15:08:22 -0300

    FEATURE: Se modifica el formulario de contacto

--- src/app/heroes-module/components/hero-profile/heroe-profile.component.ts
+++ src/app/heroes-module/components/hero-profile/heroe-profile.component.ts
@@ -21,9 +21,6 @@ export class HeroeProfileComponent implements OnInit {
   public team: string = "";
   
 
-  public message: FormControl;
-  public miGrupo: FormGroup;
-
   constructor(
     private route: ActivatedRoute,
     private heroesService: HeroesService,
@@ -52,18 +49,6 @@ export class HeroeProfileComponent implements OnInit {
             //this.heroe = new Heroe(id, name, description, modified, thumbnail, resourceURI, this.heroesService.getTeamColor(id));
           });
       });
-      
-
-      //BASIC RACTIVE FORM
-      this.message = new FormControl('', [Validators.required]);
-      this.message.setValidators(Validators.maxLength(5));
-
-      
-      //REACTIVE FORM
-      this.miGrupo = this.formBuilder.group({
-        phone: [0, Validators.compose([Validators.required, Validators.min(0)])],
-        email: ['', Validators.compose([Validators.required, Validators.email])]
-      });
 
   }
 
@@ -77,25 +62,6 @@ export class HeroeProfileComponent implements OnInit {
   getTeam(team): void {
     this.team = team;
     this.heroesService.teams.set(this.heroe.id, this.team);
-  }
-
-  onSubmit() {
-    console.log("-----------------------");
-    console.log("MENSAJE", this.message);
-    console.log("VALORES", this.message.value);
-    console.log("VALIDACION", this.message.valid);
-    console.log("VALIDACION", this.message.errors);
-  }
-  
-  handleSubmit() {
-    console.log("-----------------------");
-    console.log("Mi GRUPO", this.miGrupo);
-    console.log("CONTROLADORES", this.miGrupo.controls);
-    console.log("VALORES", this.miGrupo.value);
-    console.log("VALIDACION", this.miGrupo.valid);
-    console.log("VALIDACION", this.miGrupo);
-    this.message.setValue('Cambio el mensaje');
-
   }
 
 }
