99b08a97325c87bfe3ae6fbf1db8b4d73630f44a
Tjier Salazar <tjier.salazarvaros@gmail.com>
2020-10-07 23:33:20 -0300

    FEATURE: Implementación de redux en heroes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-
+// NGRX
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './store/app.reducers';
+// COMPONENTS
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+// MODULES
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes-module/heroes.module';
-import { ContactoModule } from './contacto/contacto.module';
 import { SharedModule } from './shared/shared.module';
+import { ContactoModule } from './contacto/contacto.module';
+import { EffectsArray } from './store/effects/index';
 
 @NgModule({
   declarations: [
@@ -17,7 +24,13 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     HeroesModule,
     ContactoModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forRoot( appReducers ),
+    EffectsModule.forRoot(EffectsArray),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/heroes-module/heroes.component.ts
+++ src/app/heroes-module/heroes.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { AppState } from '../store/app.reducers';
 import { Heroe } from '../shared/classes/heroe';
 import { HeroesService } from '../shared/services/heroes.service';
-import { map } from 'rxjs/operators';
+import { getHeroes } from '../store/actions';
 
 @Component({
   selector: 'app-heroes',
@@ -14,26 +17,33 @@ export class HeroesComponent implements OnInit {
   public heroes: Heroe[] = [];
   public searchString: string;
 
-  constructor(public heroesService: HeroesService) { }
+  constructor(
+    private store: Store<AppState>,
+    public heroesService: HeroesService
+  ) { }
 
-  ngOnInit(): void { this.submitSearch(0) }
+  ngOnInit(): void { 
+    this.submitSearch(0);
+  }
 
   submitSearch(page?: number) {
-    this.heroesService
-      .getHeroes(this.searchString, page)
-      .subscribe(({total, data}) => { 
-        console.log(total, this.heroesService.step, data);
-        this.heroes = data.map(({id, name, description, modified, thumbnail, resourceURI, teamColor, ...rest}) => new Heroe(id, name, description, modified, thumbnail, resourceURI, this.heroesService.getTeamColor(id)))
-        this.heroesService.total = Math.ceil(total / this.heroesService.step);
-      });
+    // this.heroesService
+    //   .getHeroes(this.searchString, page)
+    //   .subscribe(({total, data}) => { 
+    //     console.log(total, this.heroesService.step, data);
+    //     this.heroes = data.map(({id, name, description, modified, thumbnail, resourceURI, teamColor, ...rest}) => new Heroe(id, name, description, modified, thumbnail, resourceURI, this.heroesService.getTeamColor(id)))
+    //     this.heroesService.total = Math.ceil(total / this.heroesService.step);
+    //   });
+    this.store.select('heroes').subscribe(({ heroes }) => this.heroes = heroes);
+    this.store.dispatch( getHeroes() );
   }
 
   prevPage() {
-    this.submitSearch(this.heroesService.page - 1)
+    //this.submitSearch(this.heroesService.page - 1)
   }
 
   nextPage() {
-    this.submitSearch(this.heroesService.page + 1);
+    //this.submitSearch(this.heroesService.page + 1);
   }
 
 }
